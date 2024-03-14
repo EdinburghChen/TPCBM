b5d2702072a132f8d8f25c369e5f2a70b18ea789
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2024-03-14 15:59:57 +0800

    Update TCarList.py

--- pages/TCarList.py
+++ pages/TCarList.py
@@ -16,10 +16,28 @@ def QueryTParking():
     except Exception as e:
         st.write("Error: %s" % e)
 
+def DeleteTParking(incarno):
+    try:
+        cursor=conn.cursor()
+        sqlQueryStr='DELETE FROM 臨停車位 WHERE 車牌號碼 = ' + "'" + incarno +"';"
+        #sqlQueryStr='DELETE FROM 臨停車位 WHERE 車牌號碼 = ?;'
+        cursor.execute(sqlQueryStr)  
+        conn.commit()
+        cursor.close()
+        del cursor
+
+    except Exception as e:
+        st.write("Error: %s" % e)
 
 st.set_page_config(page_title="臨時停車即時資訊", page_icon="📈")
 
 st.markdown("### 臨時停車即時資訊")
 st.sidebar.header("臨時停車即時資訊")
 QueryTParking()
+
+inputcarno=st.text_input('輸入車牌：')
+if st.button('臨停離場', type="primary"):
+    DeleteTParking(inputcarno)
+    st.write(f':red[{inputcarno}] 離場')
+    QueryTParking()
 
