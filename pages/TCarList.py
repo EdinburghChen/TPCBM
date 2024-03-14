8345d88fe93cf601a7e85ed7c771ca6906e8b36b
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2024-03-14 15:30:22 +0800

    Update TCarList.py

--- pages/TCarList.py
+++ pages/TCarList.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import sqlite3
+
+conn=sqlite3.connect("./db/bmdb.db")
+
+def QueryTParking():
+    try:
+        cursor=conn.cursor()
+        sqlQueryStr="SELECT 車牌號碼,車位號碼,填表日期 FROM 臨停車位;"
+        cursor.execute(sqlQueryStr)    
+        for row in cursor.fetchall():
+            st.write(row)
+        cursor.close()
+        del cursor
+
+    except Exception as e:
+        st.write("Error: %s" % e)
+
 
 st.set_page_config(page_title="臨時停車即時資訊", page_icon="📈")
 
 st.markdown("### 臨時停車即時資訊")
 st.sidebar.header("臨時停車即時資訊")
+QueryTParking()
+
