93207af363f8025df09d03804795f6b1d0ae986d
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2024-03-14 16:51:59 +0800

    Update Tparking.py

--- Tparking.py
+++ Tparking.py
@@ -4,6 +4,9 @@ import sqlite3
 import requests
 from pytz import timezone
 
+hide_streamlit_style = """<style>[data-testid="stToolbar"] {visibility: hidden !important;}footer {visibility: hidden !important;}</style>"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 conn=sqlite3.connect("./db/bmdb.db")
 
 # 要發送的訊息
